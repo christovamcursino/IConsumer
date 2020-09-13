ce4f4b18b71ceaca23c8258f5120c74d0c55a372
Christovam Cursino de Oliveira <christovam.cursino@al.infnet.edu.br>
2020-09-13 17:43:33 -0300

    Api

--- IConsumer.Microservices.OrderMicroservice.CrossCutting/Injections.cs
+++ IConsumer.Microservices.OrderMicroservice.CrossCutting/Injections.cs
@@ -1,10 +1,37 @@
-﻿using System;
+﻿using IConsumer.Microservices.Common.Domain.UoW;
+using IConsumer.Microservices.Common.Infra.DataAccess.UoW;
+using IConsumer.Microservices.OrderMicroservice.Domain.AggregatesModel.OrderAggregate;
+using IConsumer.Microservices.OrderMicroservice.Infra.DataAccess.Context;
+using IConsumer.Microservices.OrderMicroservice.Infra.DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace IConsumer.Microservices.OrderMicroservice.CrossCutting
 {
-    public class Injections
+    public static class Injections
     {
+        public static void AddServices(this IServiceCollection services)
+        {
+            //Repositories
+            services.AddScoped<IOrderItemRepository, OrderItemRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IOrderTrackingRepository, OrderTrackingRepository>();
+
+            //Services
+            services.AddScoped<IOrderService, OrderService>();
+            services.AddScoped<IOrderStatusService, OrderStatusService>();
+        }
+
+        public static void AddDbContext(this IServiceCollection services)
+        {
+            //DBContext
+            services.AddScoped<DbContext, OrderContext>();
+
+            //IoW
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
     }
 }
